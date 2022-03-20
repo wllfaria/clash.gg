1f957b68f20750b405e3b183a9822ba329b79992
Willians Faria <dev.willians.faria@gmail.com>
2022-03-20 17:02:55 -0300

    feat: adding GoButton with stories

---
+++ src/components/GoButton/GoButton.stories.tsx
@@ -0,0 +1,22 @@
+import { ComponentStory } from '@storybook/react';
+import { GoButton } from '.';
+
+export default {
+	title: 'Components/GoButton',
+	component: GoButton,
+	argTypes: {
+		onClick: { action: 'clicked' }
+	}
+}
+
+const Template: ComponentStory<typeof GoButton> = (args) => <GoButton {...args} />
+
+export const Enabled = Template.bind({})
+Enabled.args = {
+	disabled: false
+}
+
+export const Disabled = Template.bind({})
+Disabled.args = {
+	disabled: true
+}
